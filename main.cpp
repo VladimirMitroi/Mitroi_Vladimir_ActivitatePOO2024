7c4020c1e3d528b257f0df753cf8d2e615db4ef4
Mitroi Alexandru-Vladimir <152998289+VladimirMitroi@users.noreply.github.com>
2024-10-10 13:35:35 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,2 +1,34 @@
-#include <iostream>
+#include<iostream>
 
+using namespace std;
+
+struct Pereche {
+    int x;
+    int y;
+};
+
+int suma(Pereche p) {
+    return p.x+p.y;
+}
+
+int inmultire(Pereche p) {
+    return p.x*p.y;
+}
+
+int diferenta(Pereche p) {
+    return p.x-p.y;
+}
+
+int main() {
+    Pereche p1;
+    cin >> p1.x;
+    cin >> p1.y;
+    cout << suma(p1);
+    
+    Pereche p2;
+    cin >> p2.x;
+    cin >> p2.y;
+    cout << suma(p2);
+    
+    return 10;
+}
