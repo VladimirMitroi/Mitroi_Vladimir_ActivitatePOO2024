3a57da3aa941a9929730458801f0b173e6f8319b
Mitroi Alexandru-Vladimir <152998289+VladimirMitroi@users.noreply.github.com>
2024-10-10 13:36:08 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,73 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-struct Pereche {
-    int x;
-    int y;
+struct CoordonateGPS {
+    int latitudine;
+    int longitudine;
+    float altitudine;
+    string locatie;
 };
 
-int suma(Pereche p) {
-    return p.x+p.y;
+void afisareCoordonateGPS(CoordonateGPS l) {
+    cout<< "Latitudine: " << l.latitudine << endl;
+    cout<< "Longitudine: " << l.longitudine << endl;
+    cout<< "Altitudine: " << l.altitudine << endl;
+    cout<< "Locatie: " << l.locatie << endl;
 }
 
-int inmultire(Pereche p) {
-    return p.x*p.y;
+void modificaAltitudine(CoordonateGPS *l, float nouaAltitudine){
+    l->altitudine=nouaAltitudine; //-> = dereferentiere+accesare
 }
 
-int diferenta(Pereche p) {
-    return p.x-p.y;
+void modificaAltitudineRef(CoordonateGPS& l, float nouaAltitudine){
+    l.altitudine=nouaAltitudine;
 }
 
-int main() {
-    Pereche p1;
-    cin >> p1.x;
-    cin >> p1.y;
-    cout << suma(p1);
+CoordonateGPS initializareCoordonate(int latitudine, int longitudine, float altitudine, string locatie){
+    CoordonateGPS l;
+    l.latitudine=latitudine;
+    l.longitudine=longitudine;
+    l.altitudine=altitudine;
+    l.locatie=locatie;
+    return l;
+}
+
+int main(){
+    CoordonateGPS l;
+    l.latitudine=46;
+    l.longitudine=22;
+    l.altitudine=1010;
+    l.locatie="Predeal";
     
-    Pereche p2;
-    cin >> p2.x;
-    cin >> p2.y;
-    cout << suma(p2);
+    afisareCoordonateGPS(l);
     
-    return 10;
+    CoordonateGPS l2;
+    cout << "Introduceti latitudinea: ";
+    cin >> l2.latitudine;
+    cout << "Introduceti longitudinea: ";
+    cin >> l2.longitudine;
+    cout << "Introduceti altitudinea: ";
+    cin >> l2.altitudine;
+    cout << "Introduceti locatia: ";
+    cin >> l2.locatie;
+    
+    afisareCoordonateGPS(l2);
+        
+    modificaAltitudine(&l2, 2000);
+    
+    afisareCoordonateGPS(l2);
+    
+    modificaAltitudineRef(l2, 3000);
+    
+    afisareCoordonateGPS(l2);
+    
+   // int a=20;
+   // int* pa=&a; //pointer
+   // int* p=new int(50); //aloca spatiu pentru 50 si ii da adresa lui p
+    
+    return 0;
 }
+
+
+//Funcția are (în ordine): tip returnat nume [parametri] { corp }
