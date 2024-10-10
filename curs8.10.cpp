d19a5d26869aa6e028905cf64d406007116d7995
Mitroi Alexandru-Vladimir <152998289+VladimirMitroi@users.noreply.github.com>
2024-10-10 13:39:51 +0300

    Add files via upload

---
+++ curs8.10.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a=10;
+    int* pa=&a;
+    cout<<pa<<endl;
+    pa++;
+    cout<<pa<<endl;
+    pa=pa-1;
+    cout<<pa<<endl;
+    int** ppa=&pa;
+    
+    double d=20.4;
+    double* pd=&d;
+    cout << ppa <<" - " << &ppa << " - " << *(*ppa)<<endl;
+    
+    int* v1;
+    int* v2;
+    
+    //v1 = (int*)malloc(sizeof(int));
+    v1=new int(8);
+    v2=new int[3] {2, 6, 8};
+    
+    delete v1;
+    delete []v2;
+    
+    return 0;
+}
